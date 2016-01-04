58eb0a60762a79359b8f42a45f48763bdf7ca42a
kolr <kol-radik@yandex.ru>
2016-01-04 12:57:56 +0200

    Added table repository

---
+++ src/main/java/app/repositories/TableRepository.java
@@ -0,0 +1,10 @@
+package app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Vocabulary(v3)
+ * Created by Родион on 03.01.2016.
+ */
+public interface TableRepository extends JpaRepository {
+}
